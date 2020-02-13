f9e6bb1e4bae93f80263acbc50aa1159ff58abf8
Shadman Kaif <kaifshadman@gmail.com>
2020-02-13 17:22:47 -0500

    Lab 3

--- ARM Cortex A9 System/part3.s
+++ ARM Cortex A9 System/part3.s
@@ -6,23 +6,32 @@ _start:
             MOV     R4, #RESULT     // R4 points to result location
             LDR     R0, [R4, #4]    // R0 holds the number of elements in the list
             MOV     R1, #NUMBERS    // R1 points to the start of the list
-            BL      LARGE           
-            STR     R0, [R4]        // R0 holds the subroutine return value
+            BL      LARGE           // Go to Large, store PC value in LR
+            MOV	    R4, R0          // R0 holds the largest value in the list after the return from subroutine
 
 END:        B       END             
 
 /* Subroutine to find the largest integer in a list
- * Parameters: R0 has the number of elements in the lisst
+ * Parameters: R0 has the number of elements in the list
  *             R1 has the address of the start of the list
  * Returns: R0 returns the largest item in the list
  */
-LARGE:      . . .
-            . . .
+
+LARGE:      MOV R3, R0 		// R3 points to number of elements in the list, allowing R0 to hold the largest number
+	    LDR R0, [R1] 	// R0 holds first number in list
+			
+LOOP: 		
+	    SUBS 	R3, #1  	// Decrement R3 by 1 
+	    MOVEQ 	PC, LR		// Checks if the loop is done, returns to main if it is done
+	    LDR		R2, [R1], #4 	// Load R2 with next value in array
+	    CMP		R0, R2		// Compare R0 with R2
+	    BGE		LOOP		// If R0 - R2 is greater than equal to 0, start at LOOP again	
+	    MOV		R0, R2		// if R2 > R0, R0 points to R2
+	    B		LOOP		// Loop again
 
 RESULT:     .word   0           
-N:          .word   7           // number of entries in the list
-NUMBERS:    .word   4, 5, 3, 6  // the data
+N:          .word   7              // number of entries in the list
+NUMBERS:    .word   4, 5, 3, 6     // the data
             .word   1, 8, 2                 
 
-            .end                            
-
+            .end     
--- ARM Cortex A9 System/part4.s
+++ ARM Cortex A9 System/part4.s
@@ -1,29 +1,46 @@
 /* Program that converts a binary number to decimal */
-           .text               // executable code follows
-           .global _start
+.text // executable code follows
+.global _start
+
 _start:
-            MOV    R4, #N
-            MOV    R5, #Digits  // R5 points to the decimal digits storage location
-            LDR    R4, [R4]     // R4 holds N
-            MOV    R0, R4       // parameter for DIVIDE goes in R0
-            BL     DIVIDE
-            STRB   R1, [R5, #1] // Tens digit is now in R1
-            STRB   R0, [R5]     // Ones digit is in R0
-END:        B      END
+			MOV    R4, #N
+			MOV    R5, #Digits     // R5 points to the decimal digits storage location
+			LDR    R4, [R4]        // R4 holds N
+			MOV    R0, R4          // parameter for DIVIDE goes in R0
+			
+			MOV    R1, #1000       // 1000 used for division for 4 digits
+			
+			BL     DIVIDE          // Divide using parameter
+			
+			STRB   R1, [R5, #3]    // Stores quotient in first place holder
+			MOV    R1, #100        // Changes parameter to 100
+			BL     DIVIDE          // Divides using new parameter
+			STRB   R1, [R5,#2]     // Stores quotient into the second placeholder
+			MOV    R1, #10         // Changes parameter to 10
+			BL     DIVIDE          // Divides using new parameter
+
+			STRB   R1, [R5, #1]    // Tens digit is now in R1
+			STRB   R0, [R5]        // Ones digit is in R0
+
+END: B END
 
 /* Subroutine to perform the integer division R0 / 10.
- * Returns: quotient in R1, and remainder in R0
+* Returns: quotient in R1, and remainder in R0
 */
+
 DIVIDE:     MOV    R2, #0
-CONT:       CMP    R0, #10
-            BLT    DIV_END
-            SUB    R0, #10
-            ADD    R2, #1
-            B      CONT
-DIV_END:    MOV    R1, R2     // quotient in R1 (remainder in R0)
-            MOV    PC, LR
 
-N:          .word  76         // the decimal number to be converted
-Digits:     .space 4          // storage space for the decimal digits
+CONT:   	CMP    R0, R1          // Compares R0 and R1
+			BLT    DIV_END         // If result is less than 0, DIV_END
+			SUB    R0, R1          // Subtract R1 from R0
+			ADD    R2, #1          // Adds 1 to R2 to see how many R1's can fit in R0
+			B      CONT            // Repeat CONT until R0 is less than R1
 
-            .end
+
+DIV_END:    MOV    R1, R2          // quotient in R1 (remainder in R0)
+			MOV    PC, LR
+			
+N: 			.word  76              // the decimal number to be converted
+
+Digits:     .space 4               // storage space for the decimal digits
+			.end
